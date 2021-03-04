eee9c7cba4d3772aaca2544ef5761526c8beecd4
thaliaamiraa <thalia02amira@gmail.com>
2021-03-04 19:29:34 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/about', function () {
+// 	echo "Nama : Thalia Amira Rifda <br> NIM : 2041723005";
+// });
+
+Route::get('/articles/{id}',function($id){
+	echo "Halaman Artikel ".$id;
+});
